ee5a6f51243e62f9498121debacc37588ef55cf1
Rokas Krivaitis <rokas@brackets.sh>
2026-03-01 20:50:04 +0200

    feat: Add additional options and keymaps for gitsigns

--- config/plugins/gitsigns.nix
+++ config/plugins/gitsigns.nix
@@ -1,5 +1,23 @@
 {
   plugins.gitsigns = {
     enable = true;
+    settings = {
+      current_line_blame = true;
+    };
   };
+
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>gb";
+      action = "<cmd>Gitsigns blame<CR>";
+      options.desc = "Show Git blame";
+    }
+    {
+      mode = "n";
+      key = "<leader>gd";
+      action = "<cmd>Gitsigns diffthis<CR>";
+      options.desc = "Show Git diff";
+    }
+  ];
 }

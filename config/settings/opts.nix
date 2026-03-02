8537e71ea221c325e5dbb275fe0d8ea944745b27
Rokas Krivaitis <rokas@brackets.sh>
2026-03-02 02:54:26 +0200

    feat: Add helm template detection

--- config/settings/opts.nix
+++ config/settings/opts.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 {
   opts = {
     number = true;
@@ -14,5 +15,26 @@
     cindent = false;
     autoindent = true;
     inccommand = "split";
+  };
+
+  filetype.extension = {
+    tpl = "helm";
+    yaml = lib.nixvim.mkRaw ''
+      function(path, bufnr)
+        if path:match('/templates/') then
+          local chart_root = vim.fs.find('Chart.yaml', {
+            path = vim.fs.dirname(path),
+            upward = true,
+            stop = vim.env.HOME
+          })
+
+          if #chart_root > 0 then
+            return 'helm'
+          end
+        end
+
+        return 'yaml'
+      end
+    '';
   };
 }
